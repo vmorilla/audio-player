cfa7dd32439cc7b66b5034ebdb797ffc257e9006
Víctor Morilla <vmorilla@gmail.com>
2025-12-20 11:29:42 +0100

    32 kHz

--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@
 int main(void)
 {
     hardware_interrupt_mode();
-    set_sound_samples_interrupt_rate(16); // 16 kHz
+    set_sound_samples_interrupt_rate(32); // 32 kHz
     if (play_sound_file("music/intro.raw") == -1)
     {
         printf("Error loading sound file!");
