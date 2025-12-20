82489bc91389eafc009f9e081d12b1fcbd296117
Víctor Morilla <vmorilla@gmail.com>
2025-12-20 15:36:28 +0100

    Better main

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <arch/zxn.h>
 #include <arch/zxn/esxdos.h>
 
 #include "debug.h"
@@ -12,19 +13,33 @@
 
 int main(void)
 {
+    zx_cls(PAPER_WHITE);
     hardware_interrupt_mode();
     set_sound_samples_interrupt_rate(32); // 32 kHz
-    if (play_sound_file("music/intro.raw") == -1)
-    {
-        printf("Error loading sound file!");
-    }
-    else
-    {
-        add_interrupt_handler(INT_CTC_CHANNEL_0, sound_interrupt_handler);
-    }
+    // if (play_sound_file("music/intro.raw") == -1)
+    // {
+    //     printf("Error loading sound file!");
+    // }
+    // else
+    // {
+    //     add_interrupt_handler(INT_CTC_CHANNEL_0, sound_interrupt_handler);
+    // }
 
     while (1)
     {
-        intrinsic_halt();
+        char key = in_inkey();
+        // Avoid processing repeated keys
+        if (key == 'i')
+        {
+            printf("Playing intro...\n");
+            play_sound_file("music/intro.raw");
+            in_wait_nokey();
+        }
+        if (key == 'l')
+        {
+            printf("Playing loop...\n");
+            play_sound_file("music/loop.raw");
+            in_wait_nokey();
+        }
     }
 }
