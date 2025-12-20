a08d7700fd0e13b6cf78397b570ca9a440ee2b99
Víctor Morilla <vmorilla@gmail.com>
2025-12-20 09:36:03 +0100

    Comments

--- src/sound_c.c
+++ src/sound_c.c
@@ -15,5 +15,5 @@ void set_sound_samples_interrupt_rate(uint8_t freqKHz)
 
     // Interrup rate to check if a buffer needs to be loaded from disk
     IO_CTC1 = 0b10000101;
-    IO_CTC1 = 255;
+    IO_CTC1 = 255; // Slowest frequency possible with CTCs: 28MHz / (16 * (1 + 255)) = ~6860 Hz
 }
